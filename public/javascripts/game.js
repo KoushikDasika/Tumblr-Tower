5926b565ad5f266ecc4e9735ce457c52eebe026a
Koushik Dasika <koushikdasika@yahoo.com>
2013-01-12 12:04:21 -0500

    converted the entire thing to a node app

---
+++ public/javascripts/game.js
@@ -0,0 +1,10 @@
+var width = 640,
+    height = 960,
+    gLoop,
+    points = 0,
+    state = true,
+    c = document.getElementById('canvas'),
+    ctx = c.getContext('2d');
+
+c.width = width;
+c.height = height;
